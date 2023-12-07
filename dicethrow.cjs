0ce069aec551e4ba351bb408baaf2dc180ef40f8
dci-student <fabian.kruse82@gmail.com>
2023-12-07 12:33:21 +0100

    build 3d6 rolls

---
+++ dicethrow.cjs
@@ -0,0 +1,23 @@
+function rollDie() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function generateAttributes() {
+  const attributes = ["STR", "DEX", "CON", "INT", "WIS", "CHA"];
+  const attributeValues = [];
+
+  for (let i = 0; i < attributes.length; i++) {
+    // Sum the result of 3 dice rolls
+    const sumOfDice = rollDie() + rollDie() + rollDie();
+    attributeValues.push(sumOfDice);
+  }
+
+  const result = attributes.map((attribute, index) => ({
+    [attribute]: attributeValues[index],
+  }));
+
+  return result;
+}
+
+const attributesArray = generateAttributes();
+console.log(attributesArray);
