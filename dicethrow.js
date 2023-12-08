f7c15418396fd094102f487ab25c3f0a0f4cae72
dci-student <fabian.kruse82@gmail.com>
2023-12-08 12:08:23 +0100

    basic race choice integrated

--- dicethrow.js
+++ dicethrow.js
@@ -44,15 +44,63 @@ if (fs.existsSync("attributesResult.txt")) {
     const existingAttributes = loadFromFile();
     console.log("Reusing existing attributes:");
     console.log(existingAttributes);
+
+    // Add the new section for choosing a race
+    chooseRace(existingAttributes);
   } else {
     const newAttributes = generateAttributes();
     console.log("Generating new attributes:");
     console.log(newAttributes);
     saveToFile(newAttributes);
+
+    // Add the new section for choosing a race
+    chooseRace(newAttributes);
   }
 } else {
   const newAttributes = generateAttributes();
   console.log("Generating new attributes:");
   console.log(newAttributes);
   saveToFile(newAttributes);
+
+  // Add the new section for choosing a race
+  chooseRace(newAttributes);
+}
+
+// Function to choose a race based on conditions
+function chooseRace(attributes) {
+  const strCondition = attributes.find((attr) => attr.STR >= 8);
+  const conCondition = attributes.find((attr) => attr.CON <= 11);
+  const dexCondition = attributes.find((attr) => attr.DEX >= 6);
+  const intCondition = attributes.find((attr) => attr.INT >= 8);
+  const chaCondition = attributes.find((attr) => attr.CHA <= 8);
+
+  console.log("Available races:");
+
+  if (strCondition && conCondition) {
+    console.log("1. Dwarf");
+  }
+  if (dexCondition && conCondition && intCondition && chaCondition) {
+    console.log("2. Elf");
+  }
+
+  console.log("3. Human (always valid)");
+
+  // Prompt user for choice
+  const raceChoice = readlineSync.questionInt(
+    "Choose your race (enter the corresponding number): "
+  );
+
+  switch (raceChoice) {
+    case 1:
+      console.log("You chose Dwarf!");
+      break;
+    case 2:
+      console.log("You chose Elf!");
+      break;
+    case 3:
+      console.log("You chose Human!");
+      break;
+    default:
+      console.log("Invalid choice. You are Human by default.");
+  }
 }
