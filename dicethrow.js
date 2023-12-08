966dae955cb30bb85af02adafcaf635b296a50c4
dci-student <fabian.kruse82@gmail.com>
2023-12-08 11:41:17 +0100

    add load & save function to txt file

--- dicethrow.js
+++ dicethrow.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const readlineSync = require("readline-sync");
 
 function rollDie() {
   return Math.floor(Math.random() * 6) + 1;
@@ -21,11 +22,37 @@ function generateAttributes() {
   return result;
 }
 
-const attributesArray = generateAttributes();
-console.log(attributesArray);
+function saveToFile(attributesArray) {
+  fs.writeFileSync(
+    "attributesResult.txt",
+    JSON.stringify(attributesArray, null, 2)
+  );
+}
 
-// Save the result to a text file
-fs.writeFileSync(
-  "attributesResult.txt",
-  JSON.stringify(attributesArray, null, 2)
-);
+function loadFromFile() {
+  const fileContent = fs.readFileSync("attributesResult.txt", "utf8");
+  return JSON.parse(fileContent);
+}
+
+// Check if the file exists
+if (fs.existsSync("attributesResult.txt")) {
+  const reuseContent = readlineSync.keyInYNStrict(
+    "There is already a character sheet saved. Reuse?"
+  );
+
+  if (reuseContent) {
+    const existingAttributes = loadFromFile();
+    console.log("Reusing existing attributes:");
+    console.log(existingAttributes);
+  } else {
+    const newAttributes = generateAttributes();
+    console.log("Generating new attributes:");
+    console.log(newAttributes);
+    saveToFile(newAttributes);
+  }
+} else {
+  const newAttributes = generateAttributes();
+  console.log("Generating new attributes:");
+  console.log(newAttributes);
+  saveToFile(newAttributes);
+}
